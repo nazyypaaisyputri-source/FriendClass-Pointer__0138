5b4bf2404c2bd29f7adc6bf945889c04e9efba7d
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-04-28 18:45:56 +0700

    implement displayAll setNama functions

--- iya.cpp
+++ iya.cpp
@@ -22,3 +22,13 @@ public:
     void setId(int pId);
     void displayAll(orang &a);
 };
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
